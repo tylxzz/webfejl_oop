47c2c578efdf49f2f63379e602fd520d29598729
tylxzz <markbencevarga@gmail.com>
2024-12-03 20:21:32 +0100

    Animal class + Bird, Mammal + (better classes)

--- scripts.js
+++ scripts.js
@@ -3,23 +3,22 @@ class Player {
         this.nickname = nickname;
         this.playedMatch = 0
     };
-}
+    
+    play() {
+        this.playedMatch++;
+        console.log(this.nickname + ` played ` + this.playedMatch + ` matches`);
+    };
 
-
-Player.prototype.play = function() {
-    this.playedMatch++;
-    console.log(this.nickname + ` played ` + this.playedMatch + ` matches`);
-};
-
-Player.prototype.getTierLevel = function(){
-    if(this.playedMatch <= 3) {
-        return 'A';
-    }
-    else if(this.playedMatch <= 6) {
-        return 'B';
-    }
-    else {
-        return 'C';
+    getTierLevel() {
+        if(this.playedMatch <= 3) {
+            return 'A';
+        }
+        else if(this.playedMatch <= 6) {
+            return 'B';
+        }
+        else {
+            return 'C';
+        }
     }
 }
 
@@ -35,11 +34,12 @@ class Person {
     constructor(name) {
         this.name = name;
     }
+
+    getName() {
+        console.log(this.name);
+    }
 }
 
-Person.prototype.getName = function() {
-    console.log(this.name);
-}
 
 class Student extends Person {
     constructor(name, school) {
@@ -52,3 +52,43 @@ const ember = new Person('Géza');
 const tanulo = new Student('Géza', 'Bolyai');
 console.log(tanulo.name, tanulo.school);
 ember.getName();
+
+class Animal {
+    constructor(name, voice) {
+        this.name = name;
+        this.voice = voice;
+    }
+
+    makeSound() {
+        console.log(`${this.name ? this.name : 'The animal'} makes a sound: ${this.voice}`);
+    }
+}
+
+class Bird extends Animal {
+    constructor(name, voice) {
+        super(name, voice);
+    }
+
+    fly() {
+        console.log(`${this.name ? this.name : 'The bird'} is flying.`);
+    }
+}
+
+class Mammal extends Animal {
+    constructor(name, voice) {
+        super(name, voice);
+    }
+
+    walk() {
+        console.log(`${this.name ? this.name : 'The mammal'} is walking.`);
+    }
+}
+
+const varju = new Bird('Odin', 'kár kár kár');
+const medve = new Mammal('Maci Laci', 'roar');
+
+varju.makeSound();
+varju.fly();
+
+medve.makeSound();
+medve.walk();
