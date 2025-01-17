3b332e7c80158260662c3fa5f321303ebecbc321
Varga Mark Bence <varga.mark@BOLYAI.LOCAL>
2025-01-17 10:06:36 +0100

    c# vagy mi

---
+++ arraylist/scripts.js
@@ -0,0 +1,43 @@
+class ArrayList {
+    /**
+     * @type {Number}
+     */
+    #count
+    #state
+    get Count() {
+        return this.#count
+    }
+    constructor() {
+        this.#count = 0
+        this.#state = {}
+    }
+    Add(element) {
+        const index = this.#count
+        this.#state[index] = element
+        Object.defineProperty(this, index, {
+            get: function() {
+                return this.#state[index]
+            },
+            set: function(value) {
+                this.#state[index] = value
+            }})
+        this.#count++
+    }
+    Clear() {
+        this.#count = 0
+        this.#state = {}
+    }
+}
+
+const alma = {}
+Object.defineProperty(alma, 'nev', {value: 'Ferenc', writable: true})
+alma.nev = 'asd'
+console.log(alma)
+
+const list = new ArrayList()
+list.Add("c1")
+console.log(list)
+list.Add("n2")
+console.log(list)
+
+console.log(list[0])
