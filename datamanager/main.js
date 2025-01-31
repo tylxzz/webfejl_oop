c5754a7fb2fbcadebd0c971f324dbab8f8882df6
Varga Mark Bence <varga.mark@BOLYAI.LOCAL>
2025-01-31 15:26:30 +0100

    datamanager, datatable

---
+++ datamanager/main.js
@@ -0,0 +1,100 @@
+/**
+ * @typedef {{nev: String, eletkor: Number}} Person
+ * 
+ * @callback Updatecalback
+ * @param {Person[]} persons
+ * @returns {void}
+ */
+class DataManager {
+    /**
+     * @type {Person[]}
+     */
+    #array
+    /**
+     * @type {Updatecalback}
+     */
+    #updatecallback
+    /**
+     * @param {Person[]} array
+     */
+    constructor(array = []) {
+        this.#array = array
+        this.#updatecallback = () => {}
+    }
+    /**
+     * @param {Updatecalback} callback
+     */
+    setUpdatecalback(callback) {
+        this.#updatecallback = callback
+        this.#updatecallback(this.#array)
+    }
+    /**
+     * 
+     * @param {Person[]} person 
+     */
+    add(person) {
+        this.#array.push(person)
+        this.#updatecallback(this.#array)
+    }
+    /**
+     * 
+     * @param {String} name 
+     */
+    filterName(name) {
+        const result = []
+        for(const elem of this.#array) {
+            if(elem.nev.includes(name)) {
+                result.push(elem)
+            }
+        }
+        this.#updatecallback(result)
+    }
+    /**
+     * 
+     * @param {Number} age 
+     */
+    filterAge(age) {
+        const result = []
+        for(const elem of this.#array) {
+            if(elem.eletkor === age) {
+                result.push(elem)
+            }
+        }
+        this.#updatecallback(result)
+    }
+}
+
+class DataTable {
+    /**
+     * 
+     * @param {DataManager} dataManager 
+     */
+    constructor(dataManager) {
+        const table = document.createElement('table')
+        document.body.appendChild(table)
+
+        const tbody = document.createElement('tbody')
+        table.appendChild(tbody)
+
+        dataManager.setUpdatecalback((persons) => {
+            tbody.innerHTML = ""
+            for(const elem of persons) {
+                const tr = document.createElement('tr')
+                tbody.appendChild(tr)
+
+                const td1 = document.createElement('td')
+                td1.innerHTML = elem.nev
+                tr.appendChild(td1)
+
+                const td2 = document.createElement('td')
+                td2.innerHTML = elem.eletkor
+                tr.appendChild(td2)
+            }
+        })
+    }
+}
+
+const dm = new DataManager([{nev: 'Sigma', eletkor: 69}, {nev: 'Ohio', eletkor: 9}, {nev: 'Fanum Tax', eletkor: 1}])
+console.log(dm)
+
+const dt = new DataTable(dm)
